0618422257c9ad32a1b90f9880072688b052cd37
k0etsu <k0etsu@k0etsu.com>
2022-12-27 09:20:22 +0000

    cleanup old template files

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import XIVodreview from "../XIVodreview.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,26 +7,8 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: XIVodreview,
     },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/fflogs",
-      name: "fflogs",
-      component: () => import("../views/report_input.vue")
-    },
-    {
-      path: "/report",
-      name: "report",
-      component: () => import("../views/report.vue")
-    }
   ],
 });
 
